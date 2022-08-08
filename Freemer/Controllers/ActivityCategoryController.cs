361cbd143ef0771e98ac22c047ac6a71853280b8
Вячеслав Литвиненко <ptb.ikc@yandex.ru>
2022-08-09 00:29:28 +0500

    Пара новых контроллеров по сущностям

---
+++ Freemer/Controllers/ActivityCategoryController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Freemer.DAL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Freemer.Controllers
+{
+    public class ActivityCategoryController : Controller
+    {
+        private readonly IActivityCategoryRepository _activityCategoryRepository;
+
+        public ActivityCategoryController(IActivityCategoryRepository activityCategoryRepository)
+        {
+            _activityCategoryRepository = activityCategoryRepository;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Freemer/Controllers/WorkOrderController.cs
@@ -0,0 +1,24 @@
+﻿using Freemer.DAL.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Freemer.Controllers
+{
+    public class WorkOrderController : Controller
+    {
+        private readonly IWorkOrderRepository _workOrderRepository;
+
+        public WorkOrderController(IWorkOrderRepository workOrderRepository)
+        {
+            _workOrderRepository = workOrderRepository;
+
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
